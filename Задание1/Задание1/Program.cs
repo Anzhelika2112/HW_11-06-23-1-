5e78f04a0c81a4a53c690d49b9ab594089edb090
Anzhelika <anjelichka2112@mail.ru>
2023-06-11 13:32:10 +0300

    Добавили задание 1

---
+++ Задание1/Задание1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите первое целое число: ");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+Console.Write("Введите второе целое число: ");
+string userInput1= Console.ReadLine() ?? "";
+int userNumber1 = int.Parse(userInput1);
+if(userNumber > userNumber1){
+    Console.WriteLine($"max = {userNumber} min = {userNumber1}");} 
+if(userNumber1 > userNumber){
+    Console.WriteLine($"max = {userNumber1} min = {userNumber}");
+}
